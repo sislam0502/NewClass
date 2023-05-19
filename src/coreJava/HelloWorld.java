5bba9bb906d0fed2ab90f3b541ba5615827cfc85
sislam0502 <sislam0502@gmail.com>
2023-05-18 21:58:43 -0400

    Home work 1

---
+++ src/coreJava/HelloWorld.java
@@ -0,0 +1,13 @@
+package coreJava;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!! Shaki");
+        System.out.println("My Name is Shaki");		
+		
+		// TODO Auto-generated method stub
+
+
+}
+}
